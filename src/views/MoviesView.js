9fe7f5c139aa4f4bc702b9a59ab8166359a01eb0
AleksandrOnis <sashaonis@gmail.com>
2021-11-28 22:54:43 +0200

    Update MoviesView.js
    add search parameter

--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { fetchSearchMovie } from '../services/moviesApi';
 import SearchForm from '../components/SearchForm/SearchForm';
@@ -8,6 +8,8 @@ function MoviesView() {
   const [movies, setMovies] = useState(null);
   const [query, setQuery] = useState('');
   const [status, setStatus] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     query && searchMovie(query);
@@ -17,6 +19,7 @@ function MoviesView() {
     setStatus('loading');
     await fetchSearchMovie(query).then(({ results }) => setMovies(results));
     setStatus(null);
+    navigate({ ...location, search: `query=${query}` });
   }
 
   const getQuery = value => setQuery(value);
